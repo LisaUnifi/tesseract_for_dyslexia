3cd7f7ce1a4349e727a0732039e883c12bdf373f
Lisa Cresti <lisa@AsusL12>
2021-10-05 12:27:06 +0200

    Update 0_oneLine.py, 10_superimposition.py, and 15 more files...

--- code/oldCode/0_oneLine.py
+++
@@ -1,6 +0,0 @@
-
-with open('C:/Users/lisac/Desktop/MarinaiProgetto/text/text-spazi.txt', 'r') as fin, open('C:/Users/lisac/Desktop/MarinaiProgetto/text/text.txt', 'w+') as fout:
-    lines = fin.readlines()
-    cleaned = [line.strip() for line in lines]
-    joined = ' '.join(cleaned)
-    fout.write(joined)
